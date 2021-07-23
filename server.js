87a468d0d152505fe20bd9b4f09354d4c04a3b5c
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-07-23 16:57:53 -0500

    Updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-adjacent-79069',
         user: 'test',
         password: '',
         database: 'smart-brain'
